97b0499bb23807bbe92ef22c5222a97a3871c42d
Chan11 <pchandrahas10@gmail.com>
2016-12-14 19:20:39 +0530

    add event listeners to listen keydown events

--- 1 JavaScript Drum Kit/index.js
+++ 1 JavaScript Drum Kit/index.js
@@ -1,7 +1,7 @@
 let keyData=[
-  {key:65,char:"A",sound:"clap"},{key:85,char:"S",sound:"hithat"},{key:68,char:"D",sound:"kick"},
+  {key:65,char:"A",sound:"clap"},{key:83,char:"S",sound:"hithat"},{key:68,char:"D",sound:"kick"},
   {key:70,char:"F",sound:"openhat"},{key:71,char:"G",sound:"boom"},{key:72,char:"H",sound:"ride"},
-  {key:74,char:"J",sound:"snare"},{key:75,char:"K",sound:"tom"},{key:76,char:"L",sound:"tink"}
+  {key:74,char:"J",sound:"snare"}
 ];
 
 let numbers=[1,2,3,4,5,6,7,8,9];
@@ -15,6 +15,13 @@ const audio= document.getElementById('audio');
                                 <kbd>${data.char}</kbd>
                                 <span class="sound">${data.sound}</span>
                                                                 </div>`);
-    audio.insertAdjacentHTML('afterbegin',`<audio data-key="${data.key}" src="http://www.soundjay.com/button/button-${i}.mp3"></audio>`)
+    audio.insertAdjacentHTML('afterbegin',`<audio data-key="${data.key}" src="http://www.soundjay.com/button/button-${i+1}.mp3"></audio>`)
   });
 })();
+
+window.addEventListener('keydown',(e)=>{
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime=0;
+  audio.play();
+})
